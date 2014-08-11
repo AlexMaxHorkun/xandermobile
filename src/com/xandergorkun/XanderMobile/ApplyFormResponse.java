b7801a259bbe1c77e9e1ceb533a682085a1fa91c
alexander.horkun <alexander.horkun@x2sy.com>
2014-08-11 16:13:23 +0300

    first commit

---
+++ src/com/xandergorkun/XanderMobile/ApplyFormResponse.java
@@ -0,0 +1,13 @@
+package com.xandergorkun.XanderMobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ApplyFormResponse extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apply_form_response);
+    }
+}
---
+++ src/com/xandergorkun/XanderMobile/HelloWorldActivity.java
@@ -0,0 +1,32 @@
+package com.xandergorkun.XanderMobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class HelloWorldActivity extends Activity {
+    public final static String EXTRA_NAME="com.xandergorkun.XanderMobile.APPLY_NAME";
+
+    public final static String EXTRA_APPLY="com.xandergorkun.XanderMobile.APPLY_AGREED";
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void sendApplyForm(View view){
+        Intent intent=new Intent(this,ApplyFormResponse.class);
+        EditText name=(EditText)view.findViewById(R.id.name);
+        RadioButton agreed=(RadioButton)view.findViewById(R.id.terms);
+        intent.putExtra(EXTRA_NAME,name.getText());
+        intent.putExtra(EXTRA_APPLY,agreed.isChecked());
+        startActivity(intent);
+    }
+}
