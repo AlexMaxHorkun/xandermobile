a1b2b54a75bd6ac3a1247e83a6216358da7338a5
alexander.horkun <alexander.horkun@x2sy.com>
2014-08-12 17:24:03 +0300

    form processing

--- src/com/xandergorkun/XanderMobile/HelloWorldActivity.java
+++ src/com/xandergorkun/XanderMobile/HelloWorldActivity.java
@@ -4,16 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.TextView;
 
 public class HelloWorldActivity extends Activity {
-    public final static String EXTRA_ERROR_NAME=HelloWorldActivity.class.toString()+".EXTRA_ERROR_NAME";
+    public final static String EXTRA_ERROR_NAME = HelloWorldActivity.class.toString() + ".EXTRA_ERROR_NAME";
 
-    public final static String EXTRA_ERROR_TERMS=HelloWorldActivity.class.toString()+".EXTRA_ERROR_TERMS";
+    public final static String EXTRA_ERROR_TERMS = HelloWorldActivity.class.toString() + ".EXTRA_ERROR_TERMS";
 
     /**
      * Called when the activity is first created.
@@ -22,26 +19,27 @@ public class HelloWorldActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Intent receivedIntent = getIntent();
+        boolean nameError = false;
+        boolean termsError = false;
+        if (receivedIntent != null) {
+            nameError = receivedIntent.getBooleanExtra(EXTRA_ERROR_NAME, false);
+            termsError = receivedIntent.getBooleanExtra(EXTRA_ERROR_TERMS, false);
+        }
+        if (!nameError) {
+            findViewById(R.id.name_error).setVisibility(View.GONE);
+        }
+        if (!termsError) {
+            findViewById(R.id.terms_error).setVisibility(View.GONE);
+        }
     }
 
-    public void sendApplyForm(View view){
-        Intent receivedIntent=getIntent();
-        if(receivedIntent!=null){
-            boolean nameError=receivedIntent.getBooleanExtra(EXTRA_ERROR_NAME,false);
-            boolean termsError=receivedIntent.getBooleanExtra(EXTRA_ERROR_TERMS,false);
-            if(nameError){
-
-            }
-            if(termsError){
-
-            }
-        }
-
-        Intent intent=new Intent(this,ApplyFormResponse.class);
-        EditText name=(EditText)findViewById(R.id.name_edit);
-        RadioButton agreed=(RadioButton)findViewById(R.id.terms_radio);
+    public void sendApplyForm(View view) {
+        Intent intent = new Intent(this, ApplyFormResponse.class);
+        EditText name = (EditText) findViewById(R.id.name_edit);
+        RadioButton agreed = (RadioButton) findViewById(R.id.terms_radio);
         intent.putExtra(ApplyFormResponse.EXTRA_NAME, name.getText().toString());
-        intent.putExtra(ApplyFormResponse.EXTRA_APPLY,agreed.isChecked());
+        intent.putExtra(ApplyFormResponse.EXTRA_APPLY, agreed.isChecked());
         startActivity(intent);
     }
 }
